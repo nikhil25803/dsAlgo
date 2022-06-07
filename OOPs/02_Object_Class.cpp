546adb842f36ca93273423bfe35dd40a98283559
Rohan Kumar Singh <rohansingh.singh70@gmail.com>
2022-06-08 02:29:41 +0530

    Comment Removed

--- OOPs/02_Object_Class.cpp
+++ OOPs/02_Object_Class.cpp
@@ -11,17 +11,17 @@ public:
 
 int main()
 {
-  // Creating an object of Box Class
+  
   Box obj;
 
-  // Assign values to object instance variable
+
   obj.width = 5;
   obj.height = 10;
   obj.depth = 15;
 
-  //Compute volume of the box
+  
   double volume = obj.width*obj.height*obj.depth;
 
-  //Printing values
+
   cout<<"Volume of a box: "<<volume<<endl;
 }
